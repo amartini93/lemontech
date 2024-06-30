191400285b517345abf4c67012ac1df46dc83330
Aldo Martini <amartini@edarkstore.cl>
2024-06-30 17:52:32 -0400

    profile update

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   has_many :registrations, dependent: :destroy
   has_many :attended_events, through: :registrations, source: :event
   has_many :created_events, class_name: 'Event', foreign_key: 'creator_id', dependent: :destroy
+
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_blank: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :users, only: [:show, :edit, :update]
+
   resources :events do
     post 'add_user', on: :member
   end
